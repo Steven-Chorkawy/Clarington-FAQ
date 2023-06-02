f41a76791d8cfce005c014596146a38ed062868f
Steven Chorkawy <schorkawy@clarington.net>
2023-06-02 09:41:21 -0400

    Fixing warnings.

--- src/pnpjs-config.ts
+++ src/pnpjs-config.ts
@@ -8,11 +8,11 @@ import "@pnp/sp/lists";
 import "@pnp/sp/items";
 import "@pnp/sp/batching";
 
-var _sp: SPFI = null;
-var _siteSP: SPFI = null;
+let _sp: SPFI = null;
+let _siteSP: SPFI = null;
 
 export const getSP = (context?: WebPartContext): SPFI => {
-  if (context != null) {
+  if (context !== null) {
     //You must add the @pnp/logging package to include the PnPLogging behavior it is no longer a peer dependency
     // The LogLevel set's at what level a message will be written to the console
     _sp = spfi().using(SPFx(context)).using(PnPLogging(LogLevel.Warning));
@@ -21,7 +21,7 @@ export const getSP = (context?: WebPartContext): SPFI => {
 };
 
 export const getSiteSP = (context?: WebPartContext, siteUrl?: string): SPFI => {
-  if (context != null) {
+  if (context !== null) {
     //You must add the @pnp/logging package to include the PnPLogging behavior it is no longer a peer dependency
     // The LogLevel set's at what level a message will be written to the console
     _siteSP = spfi(siteUrl).using(SPFx(context)).using(PnPLogging(LogLevel.Warning));
