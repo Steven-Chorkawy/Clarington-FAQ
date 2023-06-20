4e37acca0a83cf1617896dcdb040d639b0f89e5d
Steven Chorkawy <schorkawy@clarington.net>
2023-06-20 13:26:49 -0400

    Now rendering department name.

--- src/webparts/faqAccordion/components/FaqAccordion.tsx
+++ src/webparts/faqAccordion/components/FaqAccordion.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { IFaqAccordionProps } from './IFaqAccordionProps';
-//import { Accordion } from '@pnp/spfx-controls-react';
 import { getSiteSP } from '../../../pnpjs-config';
 import { RichText } from "@pnp/spfx-controls-react/lib/RichText";
 import "@pnp/sp/items/get-all";
 import "@pnp/sp/taxonomy";
 import { ExpansionPanel, ExpansionPanelActionEvent, ExpansionPanelContent } from '@progress/kendo-react-layout';
 import { Reveal } from '@progress/kendo-react-animation';
+import "@pnp/sp/taxonomy";
+
+const MOC_ORG_GROUP_ID = '4026b60c-6222-432f-b07d-89c2396e8e64';
+const DEPARTMENT_TERM_SET_ID = '8ed8c9ea-7052-4c1d-a4d7-b9c10bffea6f';
 
 export default class FaqAccordion extends React.Component<IFaqAccordionProps, any> {
 
@@ -20,10 +23,41 @@ export default class FaqAccordion extends React.Component<IFaqAccordionProps, an
     this._queryList();
   }
 
+  private async _queryDepartmentName(termID: string): Promise<void> {
+    if (!termID)
+      return;
+
+    // check if state has already been set. 
+    if (this.state[termID]) {
+      console.log(`${termID} has already been found: ${this.state[termID]}`);
+    }
+    else {
+      console.log(`${termID} NOT FOUND!`);
+      let res = await getSiteSP().termStore.groups.getById(MOC_ORG_GROUP_ID).sets.getById(DEPARTMENT_TERM_SET_ID).getTermById(termID)();
+
+      if (res.labels) {
+        if (res.labels.length > 0) {
+          // There should only ever be one item in this array so just get the first one.
+          this.setState({ [termID]: res.labels[0].name });
+
+          this.state.items.map((item: any, index: number) => {
+            if (item.Department?.TermGuid === termID) {
+              item.Department = { ...item.Department, Name: res.labels[0].name }
+            }
+          });
+        }
+      }
+    }
+  }
+
   private async _queryList(): Promise<void> {
-    getSiteSP().web.lists.getByTitle(this.props.listName).items.getAll().then(value => {
-      this.setState({ items: value });
+    let listItems = await getSiteSP().web.lists.getByTitle(this.props.listName).items.getAll();
+
+    listItems.forEach(item => {
+      this._queryDepartmentName(item.Department?.TermGuid);
     });
+
+    this.setState({ items: listItems });
   }
 
   componentDidUpdate(prevProps: Readonly<IFaqAccordionProps>, prevState: Readonly<any>, snapshot?: any): void {
@@ -44,17 +78,16 @@ export default class FaqAccordion extends React.Component<IFaqAccordionProps, an
           {this.state.items.map((item: any, index: number) => (
             <ExpansionPanel
               title={item[this.props.questionFieldName]}
-              subtitle={'subtitle'}
-              // expand none by default. 
-              //expanded={this.state.expanded === item.id}
+              subtitle={<div style={{ textAlign: 'right' }}>{item.Department?.Name}</div>}
+              expanded={this.state.expanded === item.ID}
               tabIndex={0}
-              key={item.id}
+              key={item.ID}
               onAction={(event: ExpansionPanelActionEvent) => {
-                this.setState({ expanded: event.expanded ? "" : item.id });
+                this.setState({ expanded: event.expanded ? "" : item.ID });
               }}
             >
               <Reveal>
-                {this.state.expanded === item.id && (
+                {this.state.expanded === item.ID && (
                   <ExpansionPanelContent>
                     <div className="content">
                       <span className="content-text">
@@ -65,13 +98,6 @@ export default class FaqAccordion extends React.Component<IFaqAccordionProps, an
                 )}
               </Reveal>
             </ExpansionPanel>
-            // <Accordion title={item[this.props.questionFieldName]} defaultCollapsed={true} className={"itemCell"} key={index}>
-            //   <div className={"itemContent"}>
-            //     <div className={"itemResponse"}>
-            //       <RichText value={item[this.props.answerFieldName]} isEditMode={false} />
-            //     </div>
-            //   </div>
-            // </Accordion>
           ))}
         </div>
       );
